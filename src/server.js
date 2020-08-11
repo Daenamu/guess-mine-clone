a997ac73e42cb0bd5f8a05b30459484c45c3b94a
Daenamu <dheodnr318@naver.com>
2020-08-11 22:49:55 +0900

    server Setup

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
+import { join } from "path";
 import express from "express";
+import socketIO from "socket.io";
+
+// variables
+
+const PORT = 4000;
+const app = express();
+
+// app setting and middlewares
+
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "static")));
+
+// app routing
+
+app.get("/", (req, res) => res.render("home"));
+
+// init
+const handleListening = () =>
+  console.log(`Server running: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
