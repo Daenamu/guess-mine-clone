2c05324fcfc18f3dd2a651018c5292a63cf28413
Daenamu <dheodnr318@naver.com>
2020-08-11 23:55:45 +0900

    socket io connect

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 // variables
 
@@ -12,6 +13,7 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
 app.use(express.static(join(__dirname, "static")));
+app.use(logger("dev"));
 
 // app routing
 
@@ -24,4 +26,12 @@ const handleListening = () =>
 
 const server = app.listen(PORT, handleListening);
 
-const io = socketIO(server);
+const io = socketIO.listen(server);
+
+let sockets = [];
+
+io.on("connection", (socket) => {
+  sockets.push(socket.id);
+});
+
+setInterval(() => console.log(sockets), 1000);
